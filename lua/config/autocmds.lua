77970f2552488f36df8c096ec544eb7c3261765d
Zacer559 <zacer559@gmail.com>
2025-03-10 19:47:51 +0100

    Added autosave

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -7,12 +7,12 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
--- Create an autocommand group to manage our autocmds
-vim.api.nvim_create_augroup("AutoSaveOnExitInsert", { clear = true })
--- Set up the autocommand to save files when leaving insert mode
-vim.api.nvim_create_autocmd("InsertLeave", {
-  group = "AutoSaveOnExitInsert", -- Assign to our group
-  pattern = "*", -- Apply to all file types
-  desc = "Save file on exit from insert mode", -- Description for documentation
-  command = "write", -- The command to execute (:write)
-})
+-- -- Create an autocommand group to manage our autocmds
+-- vim.api.nvim_create_augroup("AutoSaveOnExitInsert", { clear = true })
+-- -- Set up the autocommand to save files when leaving insert mode
+-- vim.api.nvim_create_autocmd("InsertLeave", {
+--   group = "AutoSaveOnExitInsert", -- Assign to our group
+--   pattern = "*", -- Apply to all file types
+--   desc = "Save file on exit from insert mode", -- Description for documentation
+--   command = "write", -- The command to execute (:write)
+-- })
